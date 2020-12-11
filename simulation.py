b7ad9e6f156fbf3a8d205df1bbd1a12dd47176f7
Gabriel Nascarella <ielnascarella@hotmail.com>
2020-12-10 23:34:23 -0300

    Adiciona comentarios

--- simulation.py
+++ simulation.py
@@ -8,10 +8,9 @@ import cv2 as cv
 # magnitude para imprimir um mapa maior
 magnitude = 8
 
-# Cria o mapa
+# Cria um mapa com landmarks aleatorios
 map = Map()
 ROBOT = '@'
-GOAL = '*'
 
 # Poe o Robo no mapa
 has_placed_robot = False
@@ -21,7 +20,7 @@ while not has_placed_robot:
 
     if map.matrix[i, j] == map.EMPTY:
         map.matrix[i, j] = ROBOT
-        robot_coord = (j, i)
+        robot_coord = (j, i) # width, height coordinates
         has_placed_robot = True
 
 # Coeficientes da equação
@@ -38,6 +37,7 @@ def coefficients(robotDistToLandmarks, landmarks, l0, l1):
 if __name__ == "__main__":
 
     picture = map.get_picture(magnitude=magnitude,  negated=1)
+    # Tentamos criar mais uma dimensao pra imagem ficar em BGR, mas falhamos
     new_pic = picture[..., np.newaxis]
 
     robx, roby = robot_coord
@@ -46,6 +46,7 @@ if __name__ == "__main__":
     landmarks = []
 
     for ID, landmark in map.landmarks:
+        # Pra cada landmark no mapa, calcula a distancia entre ela e o robo
         landx, landy = landmark
         cv.line(new_pic, (robx * magnitude, roby * magnitude), (landx*magnitude, landy*magnitude), (0,0,255))
         
@@ -54,21 +55,26 @@ if __name__ == "__main__":
         robotDistToLandmarks.append(math.hypot(diffPoints[0],diffPoints[1]))
         landmarks.append(landmark)
     
-
+    # Cria um sistema de equacoes para obter a posicao do robo
     firstCoefficients = coefficients(robotDistToLandmarks, landmarks, 0, 1)
     secondCoefficients = coefficients(robotDistToLandmarks, landmarks, 0, 2)
 
     a = np.array([firstCoefficients[:2],secondCoefficients[:2]])
     b = np.array([firstCoefficients[2], secondCoefficients[2]])
 
+    # Resolve o sistema
     pose = list(np.linalg.solve(a,b))
+    
+    # Trata a pose para obter sua posicao na imagem
     for i in range(len(pose)):
         pose[i] = magnitude * int(pose[i])
  
     print(pose)
+
+    # Plota a pose na imagem
     cv.circle(new_pic, tuple(pose), magnitude, (0,255,0), cv.FILLED)
 
-
+    # Exibe a imagem
     cv.imshow('map', new_pic)
     # cv.imwrite('results/map5.png', new_pic)
     cv.waitKey(0)
